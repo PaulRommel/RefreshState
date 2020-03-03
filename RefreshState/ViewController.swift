c51eb1f84ad52142b5a00eb840f9812904da7f46
Paul <paulrommel87@gmail.com>
2020-03-03 22:33:27 +0300

    add Indicator

--- RefreshState/ViewController.swift
+++ RefreshState/ViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-      
+    
     var timer: Timer!
-    var refreshBarButtonActivityIndicator: UIBarButtonItem!
+    var refreshIndicator: UIBarButtonItem!
     var items = [String]()
     
     @IBOutlet weak var table: UITableView!
@@ -26,24 +26,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         navigationController?.navigationBar.barStyle = .black
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshNow))
-        let activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView.init(style: .UIActivityIndicatorView.Style.medium)
-        refreshBarButtonActivityIndicator = UIBarButtonItem(customView: activityIndicator)
+
+        let activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView.init(style: UIActivityIndicatorView.Style.medium)
+        refreshIndicator = UIBarButtonItem(customView: activityIndicator)
         activityIndicator.startAnimating()
         toogleIndicator()
-
     }
+    
     deinit {
       timer?.invalidate()
     }
+    
     func toogleIndicator() {
         // 5 minutes = 300 seconds //write here 300
-        timer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(refreshNow), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: (#selector(self.refreshNow)), userInfo: nil, repeats: true)
+        
+        /*
+        self.timer = Timer.scheduledTimer(withTimeInterval: 300, repeats: true, block: {[weak self] (tt) in
+            self?.getCoinData()
+            self?.coinTableView.reloadData()
+        })
+        timer?.fire()
+ */
     }
     
     @objc func refreshNow() {
+        
         //here i am creating delay for sample purpose
             //here write your webservice code on completion of webservice change bar button item
-            self.navigationItem.rightBarButtonItem = refreshBarButtonActivityIndicator
+            self.navigationItem.rightBarButtonItem = refreshIndicator
             DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
                 self.navigationItem.rightBarButtonItem = self.navigationItem.rightBarButtonItem
             }
