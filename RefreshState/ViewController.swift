ebf1888ddc10a5d74fa113b60f389c9d7bfb8fb3
Paul <paulrommel87@gmail.com>
2020-03-15 15:04:04 +0300

    Custom Cell

--- RefreshState/ViewController.swift
+++ RefreshState/ViewController.swift
@@ -8,17 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
+    
+    let urlString = "https://script.google.com/macros/s/AKfycbxpT85ZP_8blkqMvEq0jgMBUKKrwDXnYDYVVaFoV6uot-O-hWIH/exec"
     
     var timer: Timer!
     var refreshIndicator: UIBarButtonItem!
     var items = [String]()
     
     @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupNavigatioBar()
+        let nib = UINib(nibName: "CustomCell", bundle: nil)
+        table.register(nib, forCellReuseIdentifier: "Cell")
+        
+        self.setupNavigatioBar()
+        self.downloadingJsonWithURL()
     }
     
     func setupNavigatioBar(){
@@ -40,18 +47,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func toogleIndicator() {
         // 5 minutes = 300 seconds //write here 300
         timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: (#selector(self.refreshNow)), userInfo: nil, repeats: true)
-        
-        /*
-        self.timer = Timer.scheduledTimer(withTimeInterval: 300, repeats: true, block: {[weak self] (tt) in
-            self?.getCoinData()
-            self?.coinTableView.reloadData()
-        })
-        timer?.fire()
- */
     }
     
     @objc func refreshNow() {
-        
         //here i am creating delay for sample purpose
             //here write your webservice code on completion of webservice change bar button item
             self.navigationItem.rightBarButtonItem = refreshIndicator
@@ -60,15 +58,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
     }
     
+    func downloadingJsonWithURL() {
+        guard let url = URL(string: urlString) else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url, completionHandler: { (data, response, error) in
+            if let response = response {
+                print(response)
+                
+                guard let data = data else { return }
+                print(data)
+                
+                //извлечение данных data
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }).resume()
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //let cell = table.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = table.dequeueReusableCell(withIdentifier: "Cell") as! CustomCell
         let currentStr = items[indexPath.row]
         cell.textLabel?.text = currentStr
+        //cell.myLabel.text = "Row \(indexPath.row)"
         return cell
     }
-
+    
 }
